aec6a110379b872013c956d7254caac1c2230f10
Lil_Tim <99663083+LilTim0@users.noreply.github.com>
2022-04-13 18:52:16 +0200

    2022.04.13-18:52

--- commands.py
+++ commands.py
@@ -3,40 +3,47 @@ import os
 import platform
 from traceback import print_tb
 
-def help():
+def help(arg):
+  if arg == '':
+    arg = '1'
+
   print('Displays the list of all the commands avaible')
-  print('\n|------------------------------------------------------------------------------------------------------------|')
-  print('| COMMAND                                      | DESCRIPTION                                                 |')
-  print('|------------------------------------------------------------------------------------------------------------|')
-  print('| time                                         | Displays the actual time and date                           |')
-  print('| date                                         | Displays the actual time and date                           |')
-  print('| datetime                                     | Displays the actual time and date                           |')
-  print('| [SPACE]                                      | Clears the console                                          |')
-  print('| cls                                          | Clears the console                                          |')
-  print('| clear                                        | Clears the console                                          |')
-  print('| ad <dir>                                     | Creates the  specified directory                            |')
-  print('| add-dir <dir>                                | Creates the  specified directory                            |')
-  print('| add-directory <dir>                          | Creates the  specified directory                            |')
-  print('| gt <dir>                                     | Goes to the specified directory (absolute or relative path) |')
-  print('| goto <dir>                                   | Goes to the specified directory (absolute or relative path) |')
-  print('| rd <dir>                                     | Removes the specified directory (absolute or relative path) |')
-  print('| remove-dir <dir>                             | Removes the specified directory (absolute or relative path) |')
-  print('| remove-directory <dir>                       | Removes the specified directory (absolute or relative path) |')
-  print('| le {dir}                                     | List the entries of the specified directory                 |')
-  print('| list-entries {dir}                           | List the entries of the specified directory                 |')
-  print('| ct [red] [green] [blue] [text]               | List the entries of the specified directory                 |')
-  print('| colored-text [red] [green] [blue] [text]     | List the entries of the specified directory                 |')
-  print('| -                                            | Exits the console                                           |')
-  print('| exit                                         | Exits the console                                           |')
-  print('|------------------------------------------------------------------------------------------------------------|')
-  print('\n<> : Required argument')
-  print('{} : Optionnal argument\n')
+  print('\n|------------------------------------------------------------------------------------------------------------------|')
+  print('| COMMAND                                            | DESCRIPTION                                                 |')
+  print('|------------------------------------------------------------------------------------------------------------------|')
+  if arg == '1':
+    print('| time                                               | Displays the actual time and date                           |')
+    print('| date                                               | Displays the actual time and date                           |')
+    print('| datetime                                           | Displays the actual time and date                           |')
+    print('| SPACE                                              | Clears the console                                          |')
+    print('| cls                                                | Clears the console                                          |')
+    print('| clear                                              | Clears the console                                          |')
+    print('| ad <dir>                                           | Creates the  specified directory                            |')
+    print('| add-dir <dir>                                      | Creates the  specified directory                            |')
+    print('| add-directory <dir>                                | Creates the  specified directory                            |')
+    print('| gt <dir>                                           | Goes to the specified directory (absolute or relative path) |')
+    print('| goto <dir>                                         | Goes to the specified directory (absolute or relative path) |')
+    print('| rd <dir>                                           | Removes the specified directory (absolute or relative path) |')
+    print('| remove-dir <dir>                                   | Removes the specified directory (absolute or relative path) |')
+    print('| remove-directory <dir>                             | Removes the specified directory (absolute or relative path) |')
+    print('| le {dir}                                           | List the entries of the specified directory                 |')
+    print('| list-entries {dir}                                 | List the entries of the specified directory                 |')
+    print('| ct [red] [green] [blue] [text]                     | List the entries of the specified directory                 |')
+    print('| colored-text [red] [green] [blue] [text]           | List the entries of the specified directory                 |')
+    print('| fo [file] [mode]                                   | Makes several different operations on the specified file    |')
+    print('| file-operation [file] [mode]                       | Makes several different operations on the specified file    |')
+  elif arg == '2':
+    print('| foa [file] [file2/text] [mode]                     | Makes several different operations on the specified files   |')
+    print('| file-operation-advanced [file] [file2/text] [mode] | Makes several different operations on the specified files   |')
+  print('|------------------------------------------------------------------------------------------------------------------|')
+  print('\n[] : Required argument')
+  print('{} : Optionnal argument                                                                                       Page ' + arg + '\n')
 
 
 def time():
   today = datetime.now()
   time = today.strftime("%Y/%m/%d %H:%M:%S")
-  print(time)
+  print(time)           
   
 def clear():
   os.system('cls')
@@ -95,27 +102,34 @@ def ct(r, g, b, text):
 def si(arg):
   if arg == '*':
     syst = platform.uname()
-    print('Operating System          : ' + syst[0])
-    print('Name                      : ' + syst[1])
-    print('Version                   : ' + syst[2])
-    print('Version (detail)          : ' + syst[3])
-    print('Architecture              : ' + syst[4])
-    print('Processor                 : ' + syst[5])
+    print('Operating System :\n' + syst[0])
+    print('\nName             :\n' + syst[1])
+    print('\nVersion          :\n' + syst[2])
+    print('\nVersion (detail) :\n' + syst[3])
+    print('\nArchitecture     :\n' + syst[4])
+    print('\nProcessor        :\n' + syst[5])
 
   elif arg == 'arc':
-    print('Architecture : ' + platform.machin())
+    print('Architecture :\n' + platform.machin())
   
   elif arg == 'version':
-    print('Version : ' + platform.version())
+    print('Version :\n' + platform.version())
 
   elif arg == 'osd':
-    print('Operating System (detail) : ' + platform.platform())
+    print('Operating System (detail) :\n' + platform.platform())
 
   elif arg == 'os':
-    print('Operating System : ' + platform.system())
+    print('Operating System :\n' + platform.system())
 
   elif arg == 'processor':
-    print('Processor : ' + platform.processor())
+    print('Processor :\n' + platform.processor())
+
+  elif arg == 'ldrives':
+    ldrives = ''
+    drives = [ chr(x) + ':\\' for x in range(65,91) if os.path.exists(chr(x) + ':')]
+    for i in drives:
+      ldrives += i + ' '
+    print('Logical drives :\n' + ldrives)
 
   elif arg == '?':
     print('Arguments :')
@@ -125,6 +139,7 @@ def si(arg):
     print('-osd       -> Displays the operating system detail informations')
     print('-os        -> Displays the operating system informations')
     print('-processor -> Displays the processor informations')
+    print('-ldrives   -> Displays all the accessible logical drives')
 
   else:
     print('The argument ' + arg +' doesn\'t exist, please check your syntax')
@@ -139,6 +154,8 @@ def fo(file, mode):
         lines = rf.readlines()
         for i in lines:
           text += i;
+        if text == '':
+          text = 'The file ' + file + ' is empty'
   
   elif mode == 'a':
     if not os.path.exists(file):
@@ -147,11 +164,96 @@ def fo(file, mode):
       with open(file, 'r') as rf:
         lines = rf.readlines()
         for i in lines:
-          text += i;
-      with open(file, mode) as af:
+          text += i
+      with open(file, 'w') as xf:
         temp = text
-        text = input(temp)
+        text = input('> ' + temp)
+        xf.write(temp + text)
+        text = ''
         
+        
+  elif mode == 'w':
+    if not os.path.exists(file):
+      mode = 'x'
+    with open(file, mode) as xf:
+      text = input('> ')
+      xf.write(text)
+      text = ''
+
+  elif mode == 'c':
+    with open(file, 'x') as xf:
+      xf.write('')
+
+  elif mode == 'rm':
+    if not os.path.exists(file):
+      text = 'You can\'t remove a file who does not exists'
+    else:
+      os.remove(file)
+
+  elif mode == '?':
+    print('Modes :')
+    print('-r  -> Reads the content of the specified file')
+    print('-w  -> Write some content in the specified file')
+    print('-a  -> Append some content to the specified file')
+    print('-c  -> Create the specified file')
+    print('-rm -> Removes the specified file')
+
+  return text
+
+def foa (file, sfile, mode):
+  if mode == 'ct':
+    text = ''
+    with open(sfile, 'r') as rf:
+      content = rf.readlines()
+      for i in content:
+        text += i
+
+    if not os.path.exists(file):
+      with open(file, 'x') as xf:
+        xf.write('')
+    with open(file, 'w') as wf:
+      wf.write(text)
+
+  elif mode == 'cat':
+    text = ''
+    basis = ''
+    with open(file, 'r') as rf:
+      content = rf.readlines()
+      for i in content:
+        basis += i
+
+    if not os.path.exists(sfile):
+      print('You can\'t append something to a file who does not exist')
+    else :
+      with open(sfile, 'r') as rf:
+        content = rf.readlines()
+        for i in content:
+          text += i
+
+    with open(file, 'w') as wf:
+      temp = basis + text 
+      wf.write(temp)
+
+  elif mode == 'sif':
+    cont = ''
+    with open(file, 'r') as rf:
+      content = rf.readlines()
+      for i in content:
+        cont += i
+
+    findStr = cont.replace(sfile, '')
+    find = False
+    for i in findStr:
+      if i == '':
+        find = True
+
+    if find == True:
+      print(sfile + ' has been found in ' + file)
+    else:
+       print(sfile + ' has not been found in ' + file)
+
+  else:
+    print('The argument ' + mode + ' is unknown, please check your syntax')
 
 def unknown(cmd):
   print('The command ' + cmd + ' doesn\'t exist, please check your syntax')
